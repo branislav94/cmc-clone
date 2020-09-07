3277edf1a02762763531a47a52f8ebfbcf7971ea
branislav94 <branislav94@live.com>
2020-09-07 18:08:42 +0200

    delete folder

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/market":["static\u002Fchunks\u002Fpages\u002Fmarket.js"],"/news":["static\u002Fchunks\u002Fpages\u002Fnews.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fmarket","\u002Fnews"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
