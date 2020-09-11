e4ea0c3d8561abfab25210d490e1de382569593b
branislav94 <branislav94@live.com>
2020-09-11 22:56:53 +0200

    Testing XXX 2

--- pages/porn.js
+++ pages/porn.js
@@ -9,7 +9,7 @@ const Newstest = (props) => {
 
         <div className="py-4 px-3 bg-black flex justify-between">
             <div className="flex-col text-white">
-                <h1 className="font-bold text-lg my-2">
+                <h1 className="font-bold text-xl my-2">
                     Hot Porn Videos</h1>
                 {
                     props.porn.map((data, index) => (
@@ -32,9 +32,9 @@ const Newstest = (props) => {
                             <h2
                                 className="text-gray-200 mt-2 ">
                                 {data.video.title}</h2>
-                            <div className="flex text-gray-400 mb-4">
+                            <div className="flex text-gray-400 mb-4 text-sm">
                                 <p>{data.video.views} Views</p>
-                                <p className="mx-2">Rating: {data.video.rating}%</p>
+                                <LikeIcon className="mx-2 pt-1 h-5 w-5"/><p>{data.video.rating}%</p>
                             </div>
                             {/* <p>Tags: {data.video.tags.$tag_name}</p> */}
                         </div>
@@ -59,3 +59,11 @@ Newstest.getInitialProps = async () => {
 }
 
 export default Newstest;
+
+
+function LikeIcon({ className }) {
+    return (
+<svg className={`fill-current stroke-current stroke-0 ${className}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" /></svg>
+    );
+  }
+
